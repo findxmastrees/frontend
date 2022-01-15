d0a402aa249e52cfd4ff49400bf6e16a379b6943
minjuice1 <dev.minju@gmail.com>
2022-01-15 18:30:51 +0900

    format/reviewPage_write 간단한 코드 수정

--- src/store/api/reviewApiSlice.js
+++ src/store/api/reviewApiSlice.js
@@ -29,9 +29,9 @@ export const reviewApiSlice = apiSlice.injectEndpoints({
       query: (formData) => ({
         url: '/review',
         method: 'POST',
-        body: formData,
+        body: formData
       }),
-      invalidatesTags: ['Review'],
+      invalidatesTags: [{type: 'Review'}],
     }),
   }),
 })
