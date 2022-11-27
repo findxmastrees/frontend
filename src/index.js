17315082b0293b7d2e17794a7e3e8489a7dc7428
oneny <gjwodnjs1996@gmail.com>
2022-11-27 22:20:13 +0900

    feat/redux store 생성/#7

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
+import { store } from './store'
+import { Provider } from 'react-redux'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
---
+++ src/store/slices/authSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: { displayName: null, uid: null, email: null },
+  reducers: {
+    setActiveUser: (state, action) => {
+      const { displayName, uid, email } = action.payload
+      state.displayName = displayName
+      state.uid = uid
+      state.email = email
+    },
+    logOut: (state, action) => {
+      state.user = null
+    },
+  },
+})
+
+export const { setActiveUser, logOut } = authSlice.actions
+
+export default authSlice.reducer
