35bd230f9d9605da19012159fe42615baaa55492
minjuice1 <minjuice93@gmail.com>
2022-11-28 19:14:17 +0900

    design,feat/mainPage_treeInfos 1)저장됨, 주소모달 아이콘 변경 2) 주소복사 기능 구현/#11

--- src/components/TreeInfos/index.jsx
+++ src/components/TreeInfos/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import * as S from './style'
-import AddressModal from '../common/treeInfos/addressModal'
-import TreeImages from '../common/treeInfos/treeImages'
+import AddressModal from '../common/treeInfos/AddressModal'
+import TreeImages from '../common/treeInfos/TreeImages'
 import { ReactComponent as BookMarkerIcon } from '../../assets/icons/bookmarker.svg'
 import { ReactComponent as SharingIcon } from '../../assets/icons/sharing.svg'
 import { ReactComponent as ArrowBottom } from '../../assets/icons/arrowBottom.svg'
+import { ReactComponent as ArrowUp } from '../../assets/icons/arrowUp.svg'
 
 const TreeInfos = (props) => {
   const [IsClickModal, setClickModal] = useState(false)
@@ -40,7 +41,7 @@ const TreeInfos = (props) => {
           <S.Location>{props.treeLocation}</S.Location>
           <S.Address>{props.treeAddress}</S.Address>
           <S.AddressArrow onClick={() => setClickModal(!IsClickModal)}>
-            <ArrowBottom />
+            {IsClickModal ? <ArrowUp /> : <ArrowBottom />}
           </S.AddressArrow>
         </S.AddressBox>
 
