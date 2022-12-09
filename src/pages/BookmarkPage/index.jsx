a19c3f0d687ec4f9a8f803b0447f979e1e8385ed
boksooni <8545652@naver.com>
2022-12-10 03:43:04 +0900

    rename/즐겨찾기 페이지 및 라우터 추가/#55

---
+++ src/pages/BookmarkPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import InfoHeader from '../../components/InfoHeader'
+
+export const BookmarkPage = () => {
+  return (
+    <div>
+      <InfoHeader />
+      BookmarkPage
+    </div>
+  )
+}
