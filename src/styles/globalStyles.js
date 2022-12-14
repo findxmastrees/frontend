03b3521f0dd6c61cc90b424520104bcd55d1f162
boksooni <8545652@naver.com>
2022-12-14 22:07:05 +0900

    design/수정 디자인 적응형 배경 적용/#59

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -23,18 +23,35 @@ html {
 *, *::before, *::after {
   font-family: 'Noto Sans KR', sans-serif;
   box-sizing: border-box
-
 }
 
 body {
   margin: 0;
   padding: 0;
   background-color: #ddd;
-  background-image: url('https://user-images.githubusercontent.com/91241596/204526640-4b898487-af4b-4304-ab6f-e24ba0f0ba7a.png');
+  background-image: url('https://user-images.githubusercontent.com/91241596/207074275-6c2877ac-b2c5-4924-876e-01ef269ed57a.png');
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
   background-attachment: fixed;
+}
+
+@media screen and (max-width: 1300px) {
+  body {
+    background-image: url('https://user-images.githubusercontent.com/91241596/207074992-ebaa19a1-c243-4137-858e-004c1e2b9924.png');
+  }
+}
+
+@media screen and (max-width: 1100px) {
+  body {
+    background-image: url('https://user-images.githubusercontent.com/91241596/207076488-34006564-8339-43fb-b45c-a5c476d3b4b1.png')
+  }
+}
+
+@media screen and (max-width: 850px) {
+  body {
+    background-image: url('https://user-images.githubusercontent.com/91241596/207076990-d8a7fc76-587a-4ccd-8d58-b26ad6b05d18.png')
+  }
 }
 
 li {
@@ -49,16 +66,5 @@ button {
   cursor: pointer;
 }
 
-@media screen and (max-width: 1250px) {
-  body {
-    background-image: url('https://user-images.githubusercontent.com/91241596/204527149-b54074f2-7553-4afa-9685-308569a3ebad.png');
-  }
-}
 
-@media screen and (max-width: 850px) {
-  body {
-    background-image: unset;
-    background-color: #dddfe2;
-  }
-}
 `
