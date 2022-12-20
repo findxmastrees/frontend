f1f8f4775fb948de5aa24d7c512d9a6bbb9ec035
oneny <gjwodnjs1996@gmail.com>
2022-12-21 00:25:39 +0900

    feat/트리 상세 조회 기능 구현/#70

--- src/store/api/treeApiSlice.js
+++ src/store/api/treeApiSlice.js
@@ -11,31 +11,43 @@ export const treeApiSlice = apiSlice.injectEndpoints({
         },
         method: 'GET',
       }),
-      transformErrorResponse: (responseData) => {
-        console.log(responseData)
-        // return responseData.map((data) => {
-        //   // 거리 계산
-        // })
-        return responseData
+      transformResponse: (responseData) => {
+        return responseData.data
       },
       providesTags: (result = [], error, arg) => {
         return [
           { type: 'Tree', id: 'LIST' },
-          ...result.data.map(({ tree_id }) => ({ type: 'Tree', id: tree_id })),
+          ...result.map(({ tree_id }) => ({ type: 'Tree', id: tree_id })),
         ]
       },
       async onQueryStarted(arg, { dispatch, queryFulfilled }) {
         try {
-          const {
-            data: { data },
-          } = await queryFulfilled
+          const { data } = await queryFulfilled
+          console.log(data)
           dispatch(setTrees(data))
         } catch (err) {
           console.error(err)
         }
       },
     }),
+    getTree: builder.query({
+      query: (id) => ({
+        url: `/trees/${id}`,
+      }),
+      transformResponse: (responseData) => {
+        return responseData.data
+      },
+      providesTags: (result, err, id) => [{ type: 'Tree', id }],
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled
+          console.log(data)
+        } catch (err) {
+          console.log(err)
+        }
+      },
+    }),
   }),
 })
 
-export const { useLazyGetTreesQuery } = treeApiSlice
+export const { useLazyGetTreesQuery, useGetTreeQuery } = treeApiSlice
