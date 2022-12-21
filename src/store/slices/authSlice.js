dd519dbe21a98460799c68bf9bff0e4f9fe7a7e3
oneny <gjwodnjs1996@gmail.com>
2022-12-21 23:46:13 +0900

    feat/자기 현재 위치 공통 기능 구현
    Co-authored-by: ParkMinju <minjuice1@users.noreply.github.com>
    Co-authored-by: sandwe <sandwe@users.noreply.github.com>

--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -1,8 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = {
+  displayName: null,
+  uid: null,
+  email: null,
+  myLocation: {
+    lat: 37.5100003045053,
+    lon: 127.10286871659851,
+    isLocLoading: true,
+  }
+}
+
 const authSlice = createSlice({
   name: 'auth',
-  initialState: { displayName: null, uid: null, email: null },
+  initialState,
   reducers: {
     setActiveUser: (state, action) => {
       const { displayName, uid, email } = action.payload
@@ -13,9 +24,13 @@ const authSlice = createSlice({
     logOut: (state, action) => {
       state.user = null
     },
+    setMyLocation: (state, action) => {
+      console.log('kdkdkdkkdkdkdk')
+      state.myLocation = { ...state.myLocation, ...action.payload }
+    }
   },
 })
 
-export const { setActiveUser, logOut } = authSlice.actions
+export const { setActiveUser, logOut, setMyLocation } = authSlice.actions
 
 export default authSlice.reducer
