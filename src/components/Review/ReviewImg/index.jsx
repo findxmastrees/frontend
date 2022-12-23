dc0d069fbe6f83810efe62a495429481eb4fc716
oneny <gjwodnjs1996@gmail.com>
2022-12-23 21:33:25 +0900

    refacter/리뷰 이미지 리스트 오류 수정/#86

--- src/components/Review/ReviewImg/index.jsx
+++ src/components/Review/ReviewImg/index.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import ExImage from '../../../assets/img/reviewImg.png'
 import { Image, ImgWrapper } from './style'
 
 export const ReviewImg = ({ img, review, tree_id, reviewImg, toInfo }) => {
-  const navigate = useNavigate()
+  console.log(img)
   return (
     <ImgWrapper>
       {img ? (
         <Link to='/review' state={{ review }}>
           <Image src={img} alt='Image' />
         </Link>
-      ) : (
-        <Image src={ExImage} alt='' />
-      )}
-      {reviewImg ? (
-        <Image toInfo onClick={() => navigate(`/tree/${tree_id}`)} src={reviewImg} alt='Image' />
       ) : (
         <Image src={ExImage} alt='' />
       )}
--- src/pages/MainPage/style.js
+++ src/pages/MainPage/style.js
@@ -1,9 +1,21 @@
 import styled from 'styled-components'
 
 export const Container = styled.main`
-  width: inherit;
+  max-width: 420px;
+  width: 100%;
   height: calc(100vh - 100px);
   padding: 24px 14px 0;
+  position: relative;
+  @media screen and (max-width: 390px) {
+    height: calc(100dvh - 100px);
+  }
+`
+
+export const TopWrapper = styled.div`
+  width: calc(100% - 28px);
+  margin: 0 auto;
+  position: absolute;
+  z-index: 2;
 `
 
 export const InfoSection = styled.section`
